913dc495aafdca3e552cc0794b11620de59231f3
Gomddanji <ecofolis@naver.com>
2023-10-10 10:08:57 +0900

    [Publish & Windows Service]   - publish output bin\Release\net7.0\win-64

--- Program.cs
+++ Program.cs
@@ -1,18 +1,36 @@
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+using System.Runtime.Versioning;
 using System.Security.Principal;
 
 namespace JusoroService
 {
     public class Program
     {
+        [SupportedOSPlatform("windows")]
         public static void Main(string[] args)
         {
-            IHost host = Host.CreateDefaultBuilder(args)
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+            builder.Services.AddWindowsService(option =>
+            {
+                option.ServiceName = "Jusoro Service";
+            });
+
+            LoggerProviderOptions.RegisterProviderOptions<
+                EventLogSettings, EventLogLoggerProvider>(builder.Services);
+
+            builder.Services.AddHostedService<JusoroBackgroundService>();
+
+            IHost host = builder.Build();
+
+            /*IHost host = Host.CreateDefaultBuilder(args)
                 .ConfigureServices(services =>
                 {
                     services.AddHostedService<JusoroBackgroundService>();
                 })
                 .Build();
-
+*/
             host.Run();
         }
     }
